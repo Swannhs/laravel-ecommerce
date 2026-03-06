a97c082b4de806c5826821e44863a93479890ffd
Swannhs <princeapurbask@gmail.com>
2026-03-06 16:37:14 +0600

    feat: add CurrencySeeder to populate default BDT and USD currency data.

--- app/Plugins/paypal/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Plugins\PayPal\Http\Controllers\PayPalController;
-use App\Packages\Theme\Facades\Theme;
-use Illuminate\Support\Facades\Route;
-
-Theme::registerRoutes(function (): void {
-    Route::get('payment/paypal/status', [PayPalController::class, 'getCallback'])->name('payments.paypal.status');
-});
